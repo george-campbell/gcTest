695ceca11bf5bf0ddecc01a2aec24b6d621c9860
george-campbell <george.campbell@zafin.com>
2017-10-13 13:05:45 -0400

    Added initial script.

---
+++ centos_install_deps.sh
@@ -0,0 +1,22 @@
+#!/bin/bash
+
+sed -i 's/requiretty/!requiretty/g' /etc/sudoers
+
+echo "Configure docker device marrer"
+DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | head -1 | cut -d':' -f1 )
+echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
+echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
+
+docker-storage-setup
+if [ $? -eq 0 ]
+then
+   echo "Docker thin pool logical volume created successfully"
+else
+   echo "Error creating logical volume for Docker"
+   exit 3
+fi
+
+systemctl enable docker
+systemctl start docker
+
+echo "Installation finished"
