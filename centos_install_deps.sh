5edd2909b4198e2eeca18cad7bb59394655d582a
george-campbell <george.campbell@zafin.com>
2017-10-16 11:18:54 -0400

    Update.

--- centos_install_deps.sh
+++ centos_install_deps.sh
@@ -3,8 +3,9 @@
 sed -i 's/requiretty/!requiretty/g' /etc/sudoers
 
 echo "Configure docker device marrer"
-DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/sd | head -1 | cut -d':' -f1 )
-echo 'DEVS=${DOCKERVG}\nVG=docker-vg' > /etc/sysconfig/docker-storage-setup
+DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep /dev/sd | head -1 | cut -d':' -f1 )
+echo "DEVS=${DOCKERVG}" > /etc/sysconfig/docker-storage-setup
+echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
 echo  >> /etc/sysconfig/docker-storage-setup
 
 docker-storage-setup
