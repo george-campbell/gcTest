5e319d361d9c186cc0b48020ad376391513f5136
george-campbell <george.campbell@zafin.com>
2017-10-16 10:01:44 -0400

    Update

--- centos_install_deps.sh
+++ centos_install_deps.sh
@@ -7,13 +7,6 @@ DOCKERVG=$( parted -m /dev/sda print all 2>/dev/null | grep unknown | grep /dev/
 echo "DEVS=${DOCKERVG}" >> /etc/sysconfig/docker-storage-setup
 echo "VG=docker-vg" >> /etc/sysconfig/docker-storage-setup
 
-container-storage-setup
-if [ $? -eq 0 ]
-then
-   echo "Docker thin pool logical volume created successfully"
-else
-   echo "Error creating logical volume for Docker"
-   exit 3
-fi
+docker-storage-setup
 
 echo "Installation finished"
